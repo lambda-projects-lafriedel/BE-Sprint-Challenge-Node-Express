5ca09a6dc8d79ea9b008c3fd3b7dd4a4fe771311
Leigh-Ann Friedel <leigh.ann.friedel@gmail.com>
2019-02-15 11:51:55 -0500

    Successful POST to actions and projects

--- data/routers/actionsRouter.js
+++ data/routers/actionsRouter.js
@@ -1,10 +1,11 @@
 const express = require('express');
 
 const Actions = require('../helpers/actionModel');
+const Projects = require('../helpers/projectModel');
 
 const router = express.Router();
 
-//GET to /api/projects
+//GET to /api/actions
 router.get('/', async (req, res) => {
     try {
         const actions = await Actions.get();
@@ -14,9 +15,28 @@ router.get('/', async (req, res) => {
         res.status(500).json({error: "There was an error retrieving the actions."});
     }
 })
-//GET to /api/projects/:id/actions
-//POST to /api/projects
-//PUT to /api/projects/:id
-//DELETE to /api/projects/:id
+
+//POST to /api/actions -- want to come back and fix, it doesn't return the 404 if the project id doesn't exist (but at least it doesn't add)
+router.post('/', async (req, res) => {
+    try { 
+        const { project_id, description } = req.body
+        if (!project_id || !description) {
+            res.status(400).json({message: "Both a project id and action description are required."});
+        } else {
+            const existingProject = await Projects.get(project_id);
+            if (existingProject) {
+                const newAction = await Actions.insert(req.body);
+            res.status(201).json(newAction);
+               
+            } else {
+                res.status(404).json({message: "A project with that ID does not exist."});
+            }
+        }
+    } catch {
+        res.status(500).json({error: "There was an error adding the action."});
+    }
+})
+//PUT to /api/actions/:id
+//DELETE to /api/actions/:id
 
 module.exports = router;
--- data/routers/projectsRouter.js
+++ data/routers/projectsRouter.js
@@ -15,6 +15,19 @@ router.get('/', async (req, res) => {
     }
 })
 //POST to /api/projects
+router.post('/', async (req, res) => {
+    try { 
+        const { name, description } = req.body
+        if (!name || !description) {
+            res.status(400).json({message: "Both a project name and description are required."});
+        } else {
+            const newProject = await Projects.insert(req.body);
+            res.status(201).json(newProject);
+        }
+    } catch {
+        res.status(500).json({error: "There was an error adding the project."});
+    }
+})
 //PUT to /api/projects/:id
 //DELETE to /api/projects/:id
 
